67dd1fe38ac46f0856e14f1c843f5dfba82be43b
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-01 09:59:45 -0600

    chore: cleanup from initialization

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,30 @@
 import express from 'express';
-import { authRoutes } from './auth/routes/index.routes';
 import helmet from 'helmet';
 import { errorHandler } from './middleware/error.middleware';
 import { notFoundHandler } from './middleware/notFound.middleware';
 import bodyParser from 'body-parser';
-import { express as useragent } from 'express-useragent';
 import cors from 'cors';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 
 // CORS
 app.use(
-	cors({
-		origin: true,
-		credentials: true,
-	})
+  cors({
+    origin: true,
+    credentials: true,
+  })
 );
 
 // Parse application/x-www-form-urlencoded and application/json requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Parse cookies into more usable format
-app.use(cookieParser());
 
 // Security middleware
 app.use(helmet());
 app.disable('x-powered-by'); // Disable the 'X-Powered-By' header for security (normally includes framework being used)
 
-// Useragent detection middleware
-app.use(useragent());
-
 // Register routes (anything exported from /*/routes/*.router.ts)
-app.use('/api/v1', authRoutes);
+// * app.use('/api/v1', authRoutes);
 
 // Handle 404 - Not Found
 app.use(notFoundHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import env from './types/env';
 import app from './app';
 
-const port = env.PORT || 7313;
+const port = env.PORT || 7212;
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
 });
