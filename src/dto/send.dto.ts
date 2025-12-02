2e8385abde877ebcb7310eca1c67ab5766970c3e
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-01 20:20:31 -0600

    fix: name is optional

--- src/dto/send.dto.ts
+++ src/dto/send.dto.ts
@@ -2,18 +2,18 @@ import { type ResponseBody } from '@/types/ResponseBody';
 import z from 'zod';
 
 const sendBodySchema = z.object({
-  to: z.object({
-    name: z.string(),
-    email: z.email("Must include recipient's email"),
-  }),
-  subject: z.string('Must include a subject'),
-  text: z.string('Must include alternate text to display'),
-  html: z.string('Must include a html body for the email'),
+	to: z.object({
+		name: z.string().optional(),
+		email: z.email("Must include recipient's email"),
+	}),
+	subject: z.string('Must include a subject'),
+	text: z.string('Must include alternate text to display'),
+	html: z.string('Must include a html body for the email'),
 });
 
 type SendReqBody = z.infer<typeof sendBodySchema>;
 type SendResBody = ResponseBody<{
-  message: string;
+	message: string;
 }>;
 
 export { sendBodySchema as default, type SendReqBody, type SendResBody };
