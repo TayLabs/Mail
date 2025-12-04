ac3ccc944f2e3cbd2d8befb811d5686988d38f2e
Taylor Kelley <taylorgkelley@outlook.com>
2025-12-04 14:19:56 -0600

    fix: adding graceful shutdown for express

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import env from './types/env';
 import app from './app';
 
 const port = env.PORT || 7212;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
+});
+
+// Gracefully shutdown server
+process.on('SIGTERM', async () => {
+  await server.close();
+  process.exit(0);
 });
